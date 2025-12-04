e5d954ffc056b5549ddea3b44f1f130867e81698
JuanDa0423 <juanoquendo9@gmail.com>
2025-12-04 12:56:16 -0500

    Fix Flask to use OpenShift PORT variable

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+import os
 from backend import app
 
 if __name__ == "__main__":
     print("songs application.")
-    app.run(host="0.0.0.0", port=8080, debug=True)
+
+    port = int(os.environ.get("PORT", 8080))  # OpenShift injects PORT
+    app.run(host="0.0.0.0", port=port, debug=True)
